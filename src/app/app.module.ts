7c2d966c4383cb6e7739ed8576ecc1d234919b3f
Veli S <VSithole@osiristrading.com>
2018-08-17 17:07:53 +0200

    refactor(): update media mixin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './components/grid/grid.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GridComponent
+    GridComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,

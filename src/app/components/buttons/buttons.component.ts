bfdb3b3f1b81cdbb8525d63ee5916ee1a2341852
Veli S <VSithole@osiristrading.com>
2018-08-20 09:38:29 +0200

    refactor(Config): change app prefix to plg

--- src/app/components/buttons/buttons.component.ts
+++ src/app/components/buttons/buttons.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-buttons',
+  selector: 'plg-buttons',
   templateUrl: './buttons.component.html',
   styleUrls: ['./buttons.component.scss']
 })

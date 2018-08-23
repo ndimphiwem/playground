80f9c1f7b6a6d7f51a13e814441333343619ed7d
Veli S <VSithole@osiristrading.com>
2018-08-23 09:09:32 +0200

    refactor(Theme): theme cleanup

---
+++ src/app/components/panels/panels.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'plg-panels',
+  templateUrl: './panels.component.html',
+  styleUrls: ['./panels.component.scss']
+})
+export class PanelsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
